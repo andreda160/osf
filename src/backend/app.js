b702f0fbd770e7c60425b8c6a234c95cb7be2359
Resfriado <mikaelflips@gmail.com>
2025-11-11 23:10:25 -0300

    fix: merge conflict

--- src/backend/app.js
+++ src/backend/app.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const route = '/home';
-const newPath = 'src/frontend/pages/home/index.html';
+const newPath = 'src/frontend/pages/test/index.html';
 
 app.get('/', (req, res) => {
   res.redirect(route);
